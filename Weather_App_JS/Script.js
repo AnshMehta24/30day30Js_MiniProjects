9be3b653c5afc9450eabbb6d76d0cd92cd69c516
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-03-17 17:59:31 +0530

    commit

--- Weather_App_JS/Script.js
+++ Weather_App_JS/Script.js
@@ -4,18 +4,43 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
-
+const icon = document.querySelector(".weather-icon");
+const weather = document.querySelector('.weather');
 async function checkWeather(city){
     const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
     let data  = await response.json();
 
-    console.log(data);
+
+    if(response.status === 404) alert("Wrong City Name Try writing in lowercase");
     document.querySelector('.city').innerHTML = data.name;
     document.querySelector('.temp').innerHTML =Math.round(data.main.temp) + "°C";
     document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
     document.querySelector('.wind').innerHTML = data.wind.speed + "Km/H";
+
+    if(data.weather[0].main === "Clouds"){
+            icon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main === "Clear"){
+            icon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main === "Rain"){
+            icon.src = "images/rain.png";
+    }
+   else if(data.weather[0].main === "Drizzle"){
+            icon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main === "Mist"){
+            icon.src = "images/mist.png";
+    }
+    else if(data.weather[0].main === "Snow"){
+            icon.src = "images/snow.png";
+    }
+
+    weather.style.display = "block";
+    
 }
 
 searchBtn.addEventListener('click',()=>{
+
     checkWeather(searchBox.value);
 })
