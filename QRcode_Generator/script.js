b68489ee68818bf7e46b0e9dfd484c55ac51500b
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-03-30 18:56:22 +0530

    commit

---
+++ QRcode_Generator/script.js
@@ -0,0 +1,13 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("text");
+function generateQR(){
+    if(qrText.value.length >0){
+    qrImage.src ="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img")
+    }
+    else{
+        alert("Write something to Generate Qr code")
+    }
+}
+
